86f420a84415d0989b5f173c46078484e63e15b1
Yousef joe <37766035+anajo333@users.noreply.github.com>
2021-03-30 05:09:34 +0200

    1
    1

---
+++ pr0xy.php
@@ -0,0 +1,10 @@
+<?php
+$ch = curl_init(base64_decode($_GET['get']));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)");
+curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1000);
+curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
+$output = curl_exec($ch);
